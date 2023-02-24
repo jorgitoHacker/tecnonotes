987aec607af7221ef37d6efde18db18fed8090c2
jorgitoHacker <126207070+jorgitoHacker@users.noreply.github.com>
2023-02-24 09:42:26 -0500

    Update apiSlice.js

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'https://techonotes-api-djia.onrender.com',
+    baseUrl: 'https://technotes-api-djia.onrender.com',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
